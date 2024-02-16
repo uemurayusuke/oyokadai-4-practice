f33a97c06b2dfc1b0e066bee74a3b4ca734e87df
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-02-16 07:02:07 +0000

    フォロー機能練習

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,3 +2,7 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 end
+
+#followerというのは、userテーブルに存在するイメージ
+#followedというのも、userテーブルに存在するイメージ
+#userテーブルに存在するから、class_nameでuserを指定する必要がある。
